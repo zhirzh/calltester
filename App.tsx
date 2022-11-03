8866f78f4b2102415700d43882678bc7f5b5a8d5
Shirsh Zibbu <zibbu.shirsh@gmail.com>
2022-11-03 19:47:17 +0530

    add simple caller

--- App.tsx
+++ App.tsx
@@ -1,12 +1,56 @@
-import { StatusBar } from 'expo-status-bar'
-import { StyleSheet, Text, View } from 'react-native'
+import { useRef } from 'react'
+import {
+  Button,
+  KeyboardAvoidingView,
+  Linking,
+  Platform,
+  StyleSheet,
+  Text,
+  TextInput,
+  View,
+} from 'react-native'
 
 export default function App() {
+  const phone = useRef('')
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <KeyboardAvoidingView behavior="height" style={styles.container}>
+      <View style={styles.row}>
+        <Text style={styles.text}>Phone</Text>
+        <TextInput
+          style={[styles.text, styles.input]}
+          onChangeText={(text) => {
+            phone.current = text
+          }}
+        />
+      </View>
+
+      <View style={styles.btn}>
+        <Button
+          title="Call tel"
+          onPress={async () => {
+            try {
+              await Linking.openURL(`tel:${phone.current}`)
+            } catch (e) {
+              alert(e.message)
+            }
+          }}
+        />
+      </View>
+
+      <View style={styles.btn}>
+        <Button
+          title="Call telprompt"
+          onPress={async () => {
+            try {
+              await Linking.openURL(`telprompt:${phone.current}`)
+            } catch (e) {
+              alert(e.message)
+            }
+          }}
+        />
+      </View>
+    </KeyboardAvoidingView>
   )
 }
 
@@ -14,7 +58,27 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    padding: 20,
+  },
+  text: {
+    fontSize: 16,
+  },
+  row: {
+    marginVertical: 15,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+  },
+  btn: {
+    marginVertical: Platform.select({
+      android: 10,
+    }),
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    marginLeft: 10,
+    padding: 4,
   },
 })
